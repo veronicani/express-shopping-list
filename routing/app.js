b95c8f99f748d21d06bbfd099db9703c2e454213
Veronica Ni <vni1294@gmail.com>
2024-01-03 18:42:36 -0500

    Initial commit

---
+++ routing/app.js
@@ -0,0 +1,46 @@
+/** App for shopping list. */
+
+const express = require("express");
+const { NotFoundError } = require("./expressError");
+const app = express();
+
+const userRoutes = require("./userRoutes");
+const { logger, onlyAllowElie } = require("./middleware");
+
+app.use(express.json());
+
+// this applies to all requests at all paths
+app.use(logger);
+
+// apply a prefix to every route in userRoutes
+app.use("/users", userRoutes);
+
+/** Greet user, only if it is Elie. */
+
+app.get(
+  "/hello/:name",
+  // first function -- middleware function
+  onlyAllowElie,
+  // second function -- our view function
+  function (req, res) {
+    return res.json({ "greeting": `Hello, ${req.params.name}` });
+  }
+);
+// end
+
+
+/** 404 handler: matches unmatched routes; raises NotFoundError. */
+app.use(function (req, res) {
+  throw new NotFoundError();
+});
+
+/** Error handler: logs stacktrace and returns JSON error message. */
+app.use(function (err, req, res, next) {
+  const status = err.status || 500;
+  const message = err.message;
+  if (process.env.NODE_ENV !== "test") console.error(status, err.stack);
+  return res.status(status).json({ error: { message, status } });
+});
+
+
+module.exports = app;
